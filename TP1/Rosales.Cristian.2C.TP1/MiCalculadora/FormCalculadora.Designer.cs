025b001e697716d3b228d7ec5603b83e3582d1b9
rosalescristian <rosalescristianm@gmnail.com>
2022-04-15 19:45:05 -0300

    TP1: Windows Form - Commit Inicial

---
+++ TP1/Rosales.Cristian.2C.TP1/MiCalculadora/FormCalculadora.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace MiCalculadora
+{
+    partial class FormCalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComboBoxOperando = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ComboBoxOperando
+            // 
+            this.ComboBoxOperando.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxOperando.FormattingEnabled = true;
+            this.ComboBoxOperando.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.ComboBoxOperando.Location = new System.Drawing.Point(259, 54);
+            this.ComboBoxOperando.Name = "ComboBoxOperando";
+            this.ComboBoxOperando.Size = new System.Drawing.Size(121, 23);
+            this.ComboBoxOperando.TabIndex = 0;
+            // 
+            // FormCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ComboBoxOperando);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Cristian Rosales del curso 2C";
+            this.Load += new System.EventHandler(this.FormCalculadora_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboBoxOperando;
+    }
+}
